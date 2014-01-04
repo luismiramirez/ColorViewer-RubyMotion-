c5d263658aaaa957711b15c0178cd08430aff7f6
Luismi Ramirez <luismir89@gmail.com>
2014-01-04 18:54:18 +0100

    override initwithnibname en colors y tabBar

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -5,7 +5,10 @@ class AppDelegate
     controller = ColorsController.alloc.initWithNibName(nil, bundle: nil)
     nav_controller =
       UINavigationController.alloc.initWithRootViewController(controller)
-    @window.rootViewController = nav_controller
+    tab_controller =
+      UITabBarController.alloc.initWithNibName(nil, bundle: nil)
+    tab_controller.viewControllers = [nav_controller]
+    @window.rootViewController = tab_controller
     true
   end
 end
